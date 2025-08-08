2fe540a6a2934f6f91359aad26f3b22758a14d82
Amr <amrftooy7@gmail.com>
2025-08-08 23:12:55 +0300

    Add project files.

---
+++ Range.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass6
+{
+    internal class Range<T> where T : IComparable<T>
+    {
+        public T Min { set; get; }
+        public T Max { set; get; }
+
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+        }
+
+        public int Length()
+        {
+            dynamic a = Max;
+            dynamic b = Min;
+            return a - b;
+        }
+}
+}
